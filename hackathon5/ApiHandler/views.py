771806e6a47360d30e58ac4cfb491957c5078563
anto.simon <anto.simon@GAI.NET>
2017-04-08 02:32:50 +0530

    django and few graph structures

---
+++ hackathon5/ApiHandler/views.py
@@ -0,0 +1,20 @@
+# Create your views here.
+import json
+from datetime import datetime as dt
+from django.http import HttpResponse
+
+
+def AppRateReal(request):
+    """To calculate and return the Application rate
+    Realtime"""
+
+    #if not request.method == 'POST':
+    #    return HttpResponse('Method not allowed', status=402)
+
+    #req_type = request.POST.get('type', '')
+
+    #if req_type == 'all':
+    q= """select count(*) from application_application where id like "%s%%";"""%dt.strftime(dt.now(),'%Y%d%m%H')
+
+    return HttpResponse(q)
+    
